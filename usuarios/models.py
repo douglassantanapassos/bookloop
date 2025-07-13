af0b973a74279799d281954cd46c431af86efc6a
Douglas Santana <douglassantanapassos7@gmail.com>
2025-07-13 12:15:27 -0400

    Inicio do projeto (13.07.25)

---
+++ usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Nota(models.Model):
+    nome_aluno = models.CharField(max_length = 200)
+    disciplina = models.CharField(max_length = 200)
+    nota_atividade = models.IntegerField(default= 0)
+    nota_trabalho = models.IntegerField(default=0)
+    nota_prova = models.IntegerField(default = 0)
+    media = models.FloatField(blank = True, default = 0)
